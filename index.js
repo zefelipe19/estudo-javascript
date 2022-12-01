92cea1b686974ce9008bbc8cbce2c5cc90271be0
Ze Felipe <felipe.paula1903@gmail.com>
2022-12-01 19:20:43 -0300

    fiz muitas alterações

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-const opah = () => {
-    const div01 = document.querySelector(".div01")
-    const nome = document.querySelector("#nome")
-    if (nome.value == "Felipe" | nome.value == "felipe") {
-        div01.innerHTML = `<h2>Opah ${nome.value}.</h2>`
-    } else if (nome.value == "Victor" | nome.value == "victor") {
-        div01.innerHTML = `<h4>O ${nome.value}pomba.</h4>`
-    } else {
-        div01.innerHTML = `<p><strong>Oi ${nome.value}</strong>.</p>`
-    }
-    nome.value = " "
-}
+const API_URL = "https://api.adviceslip.com/advice"
 
 
-const formulario = document.querySelector(".todo")
-formulario.onsubmit = (event) => {
-    event.preventDefault()
-
-    const input = document.querySelector("#item").value
-    const ul = document.querySelector("ul")
-    const li = document.querySelector("li").cloneNode(true)
-    ul.append(li)
+const GetConselhos = () => {
+    axios.get(API_URL)
+    .then(response => {
+        let advice = response.data
+        let conselho = document.getElementById("conselho")
+        conselho.innerHTML = `<hr><h6>${advice.slip.advice}</h6><hr>`
+    })
+    .catch(error => console.error(error))
 }
