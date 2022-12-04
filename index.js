d6853bbe080290d02f6e52907c9b5ea0735cf137
Ze Felipe <felipe.paula1903@gmail.com>
2022-12-04 02:12:42 -0300

    atualizando o css

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const API_URL = "https://api.adviceslip.com/advice"
-
+const API_SPOTIFY = "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10"
 
 const GetConselhos = () => {
     axios.get(API_URL)
     .then(response => {
-        let advice = response.data
-        let conselho = document.getElementById("conselho")
+        const advice = response.data
+        const conselho = document.getElementById("conselho")
         conselho.innerHTML = `<hr><h6>${advice.slip.advice}</h6><hr>`
     })
     .catch(error => console.error(error))
